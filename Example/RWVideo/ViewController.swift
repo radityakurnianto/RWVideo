fd47e218b9edbb1898e204fd884f8f6f3c13693d
Raditya Kurnianto <raditya@detik.com>
2019-02-07 16:44:20 +0700

    add video view

--- Example/RWVideo/ViewController.swift
+++ Example/RWVideo/ViewController.swift
@@ -7,16 +7,27 @@
 //
 
 import UIKit
+import RWVideo
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        guard let url = URL(string: "http://cdnapi.kaltura.com/p/243342/sp/24334200/playManifest/entryId/0_uka1msg4/flavorIds/1_vqhfu6uy,1_80sohj7p,1_ry9w1l0b/format/applehttp/protocol/http/a.m3u8") else { return }
+        let video = RWVideo(videoUrl: url)
+        
+        self.present(video, animated: true) {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
-
--- RWVideo/Classes/RWVideo.swift
+++ RWVideo/Classes/RWVideo.swift
@@ -18,6 +18,12 @@ open class RWVideo: UIViewController {
     fileprivate let BufferEmpty = "playbackBufferEmpty"
     fileprivate let KeepUp = "playbackLikelyToKeepUp"
     
+    fileprivate lazy var videoView: UIView = { [unowned self] in
+        let v = UIView()
+        v.backgroundColor = .blue
+        return v
+    }()
+    
     open var didTapPlay:(()->Void)?
     open var didTapPause:(()->Void)?
     
@@ -30,13 +36,9 @@ open class RWVideo: UIViewController {
         self.videoURL = videoUrl
         self.avplayer = AVPlayer(url: self.videoURL)
         self.avplayerLayer = AVPlayerLayer(player: self.avplayer)
-        self.avplayerLayer.videoGravity = .resizeAspectFill
         self.avplayerItem = self.avplayer.currentItem
         
         super.init(nibName: nil, bundle: nil)
-        
-        self.view.layer .insertSublayer(self.avplayerLayer, at: 0)
-        addObserver()
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -44,12 +46,38 @@ open class RWVideo: UIViewController {
     }
     
     // MARK: View life cycle
-    override open func viewDidLoad() {
-        super.viewDidLoad()
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        view.backgroundColor = .black
+        view.addSubview(videoView)
+        videoViewPotraitConstraint()
+        videoView.layer.insertSublayer(self.avplayerLayer, at: 0)
+        
+        addObserver()
     }
     
     override open func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
+        removeObserver()
+    }
+    
+    
+    
+    // MARK: Constraint
+    fileprivate func videoViewPotraitConstraint() -> Void {
+        videoView.translatesAutoresizingMaskIntoConstraints = false
+        videoView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        videoView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        //        videoView.topAnchor.constraint(equalTo: view.topAnchor)
+        videoView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        videoView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        videoView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 9.0/16.0).isActive = true
+//        videoView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    }
+    
+    func videoViewLandscapeConstraint() -> Void {
+        //
     }
     
     // MARK: Player command
@@ -86,10 +114,14 @@ open class RWVideo: UIViewController {
         if keypath.elementsEqual(VideoStatus) {
             switch (item.status) {
             case .unknown :
+                print("player_unknown")
                 break
             case .failed:
+                print("player_failed")
                 break
             case .readyToPlay:
+                self.play()
+                print("player_ready_toPlay")
                 break
             }
         }
@@ -104,6 +136,6 @@ open class RWVideo: UIViewController {
     }
     
     @objc fileprivate func playbackFinish(notification: Notification) {
-        
+        print("player_finished")
     }
 }
